a58883934c8ef5cdb90b8521a6db75a5a1c11f40
thanhtt <thanhtt-copilot@gmai.com>
2026-05-03 20:36:29 +0700

    add postgres lib

--- dbt-dagster-project/de-team/dagster_project/resources/__init__.py
+++ dbt-dagster-project/de-team/dagster_project/resources/__init__.py
@@ -4,8 +4,13 @@
 # Uses PipesS3MessageReader to receive logs/events from Spark via S3.
 # ---------------------------------------------------------------------------------------------------------------------
 
+import os
+
 import boto3
 from dagster_aws.pipes import PipesEMRContainersClient, PipesS3MessageReader
+
+# Ensure boto3 can find the region from AWS_REGION env var
+_REGION = os.environ.get("AWS_REGION", "ap-southeast-1")
 
 
 def create_pipes_emr_client(
@@ -17,8 +22,9 @@ def create_pipes_emr_client(
     include_stdio_in_messages=True forwards Spark driver stdout/stderr to Dagster.
     """
     return PipesEMRContainersClient(
+        client=boto3.client("emr-containers", region_name=_REGION),
         message_reader=PipesS3MessageReader(
-            client=boto3.client("s3"),
+            client=boto3.client("s3", region_name=_REGION),
             bucket=pipes_s3_bucket,
             include_stdio_in_messages=True,
         ),
